993ef216a64dd50f2e79100765ab107ba237ab5b
Víctor Hugo Campos Vázquez <vcamposvazquez@gmail.com>
2022-04-14 13:13:51 +0200

    first commit

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,14 +1,14 @@
     const parse = require('mysql-parse').parse;
-    const config = parse(process.env.DATABASE_URL);
+    const config = parse(process.env.JAWSDB_URL);
     module.exports = ({ env }) => ({
         connection: {
             client: 'mysql',
         connection: {
-            host: env('DATABASE_HOST', 'i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'),
-            port: env.int('DATABASE_PORT', 3306),
-            database: env('DATABASE_NAME', 'h8uiqcotskch7rrz'),
-            user: env('DATABASE_USERNAME', 'wfl3hcyj61om4i18'),
-            password: env('DATABASE_PASSWORD', 'joe5rt8zobrxct5a'),
+                host: config.host,
+                port: config.port,
+                database: config.database,
+                user: config.user,
+                password: config.password,
             ssl: {
               rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
             },
