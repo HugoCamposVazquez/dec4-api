822b8f50bb58ab24ba5a2b7fbc75745a00a5647d
Víctor Hugo Campos Vázquez <vcamposvazquez@gmail.com>
2022-04-16 00:26:47 +0200

    fix prod

---
+++ config/env/production/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: "hpes8lqco",
+        api_key: "177433923813475",
+        api_secret: "3ZlkiyD1jgiaxxZ0DUd0yyoFKsU",
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
