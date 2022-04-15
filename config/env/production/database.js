b7c90f9d2f8b5f8e6d38b3673e5b5af2dc7c59d7
Víctor Hugo Campos Vázquez <vcamposvazquez@gmail.com>
2022-04-15 21:37:05 +0200

    fix prod

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,15 +4,16 @@ connections: { default:
     { connector: 'bookshelf', 
     settings: { 
         client: 'mysql', 
-        host: env('DATABASE_HOST', 'localhost'), 
-        port: env.int('DATABASE_PORT', 3306), 
-        database: env('DATABASE_NAME', 'strapi'), 
-        username: env('DATABASE_USERNAME', 'strapi'), 
-        password: env('DATABASE_PASSWORD', 'strapi'), }, 
+        host: env('DATABASE_HOST', 'i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'h8uiqcotskch7rrz'),
+        user: env('DATABASE_USERNAME', 'wfl3hcyj61om4i18'),
+        password: env('DATABASE_PASSWORD', 'joe5rt8zobrxct5a'),
         ssl: {
             rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
           },
           
     }, 
 }, 
+},
 });
